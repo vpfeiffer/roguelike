08a8bc09d6ec0e814cf12f86f4536ea40c8b0f12
vpfeiffer <violetpfeiffer@gmail.com>
2022-08-27 18:44:39 +0000

    add entity

--- src/main.rs
+++ src/main.rs
@@ -27,22 +27,50 @@ struct State {
  *      type Storage = VecStorage<Self>;
  *  } */
 
-fn main() {
-    println!("Hello, world!");
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
 
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
 
+    gs.ecs
+    .create_entity()
+    .with(Position { x: 40, y: 25 })
+    .with(Renderable {
+        glyph: rltk::to_cp437('@'),
+        fg: RGB::named(rltk::YELLOW),
+        bg: RGB::named(rltk::BLACK),
+    })
+    .build();
+
     for i in 0..10 {
         gs.ecs
-            .create_entity()
-            .with(Position { x: 40, y: 25 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('@'),
-                fg: RGB::named(rltk::YELLOW),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .build();
+        .create_entity()
+        .with(Position { x: i * 7, y: 20 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('☺'),
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
     }
+    rltk::main_loop(context, gs)
 }
+
+

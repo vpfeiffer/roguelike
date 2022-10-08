d49e4b102d060262aadedd27da87521ad6bf9500
vpfeiffer <violetpfeiffer@gmail.com>
2022-10-08 00:38:46 +0000

    Add player movement

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,9 @@ struct LeftMover {}
 
 struct LeftWalker {}
 
+#[derive(Component, Debug)]
+struct Player {}
+
 struct State {
     ecs: World,
 }
@@ -30,6 +33,7 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
+        player_input(self, ctx);
         self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -37,6 +41,7 @@ impl GameState for State {
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+
     }
 }
 
@@ -54,12 +59,35 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
-
 impl State {
     fn run_systems(&mut self) {
         let mut lw = LeftWalker{};
         lw.run_now(&self.ecs);
         self.ecs.maintain();
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
     }
 }
 
@@ -80,6 +108,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     // PC
     gs.ecs
@@ -90,6 +119,7 @@ fn main() -> rltk::BError {
         fg: RGB::named(rltk::YELLOW),
         bg: RGB::named(rltk::BLACK),
     })
+    .with(Player{})
     .build();
 
     // NPCs

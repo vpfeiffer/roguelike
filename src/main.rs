85890b165eb4a304182892306402688c6dfd3aba
vpfeiffer <violetpfeiffer@gmail.com>
2022-10-19 01:50:11 +0000

    Add functionality to draw the map This adds wall and floor tiles to the map which are placed using procedural generation.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use specs::prelude::*;
 use std::cmp::{max, min};
 use specs_derive::Component;
 
+const WIDTH: usize = 80;
+const HEIGHT: usize = 50;
+
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -39,6 +42,9 @@ impl GameState for State {
         ctx.cls();
         player_input(self, ctx);
         self.run_systems();
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -74,10 +80,14 @@ impl State {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -90,15 +100,74 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
             VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
+            //VirtualKeyCode::Y => try_move_player(-1, -1, &mut gs.ecs),
+            //VirtualKeyCode::U => try_move_player(1, -1, &mut gs.ecs),
+            //VirtualKeyCode::B => try_move_player(-1, 1, &mut gs.ecs),
+            //VirtualKeyCode::N => try_move_player(1, 1, &mut gs.ecs),
             _ => {}
         },
     }
 }
 
+// For maps
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * WIDTH) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; WIDTH*HEIGHT];
+
+    // Make the boundaries walls
+    for x in 0..WIDTH as i32 { 
+        map[xy_idx(x, 0)] = TileType::Wall; 
+        map[xy_idx(x, 49)] = TileType::Wall; 
+    }
+    for y in 0..HEIGHT as i32 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls.
+    // First obtain the thread-local RNG.
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x  = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx : &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            }
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            }
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
 /* By using  #[derive(Component)] you avoid the need to type the following in addition to the
  * struct
- * impl Component for Position {
- *      type Storage = VecStorage<Self>;
+ * impl Component for Position { type Storage = VecStorage<Self>;
  *  } */
 
 
@@ -113,12 +182,10 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+    gs.ecs.insert(new_map());
 
     // PC
-    gs.ecs
-    .create_entity()
-    .with(Position { x: 40, y: 25 })
-    .with(Renderable {
+    gs.ecs .create_entity() .with(Position { x: 40, y: 25 }) .with(Renderable {
         glyph: rltk::to_cp437('@'),
         fg: RGB::named(rltk::YELLOW),
         bg: RGB::named(rltk::BLACK),

6f832612feb1eabe75e398bbbb56730e0f0794ef
vpfeiffer <violetpfeiffer@gmail.com>
2022-11-14 01:43:11 +0000

    Add viewshed component to components

--- src/components.rs
+++ src/components.rs
@@ -16,4 +16,10 @@ pub struct Renderable {
 }
 
 #[derive(Component, Debug)]
+pub struct Viewshed {
+    pub visible_tiles : Vec<rltk::Point>,
+    pub range : i32,
+}
+
+#[derive(Component, Debug)]
 pub struct Player {}

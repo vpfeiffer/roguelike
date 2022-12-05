9a2a4a777aba81e661ff2530b0c79488aba92ec1
vpfeiffer <violetpfeiffer@gmail.com>
2022-12-05 02:01:10 +0000

    Add function to print map

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use std::io::{self, Read};
+use ndarray::prelude::*;
 
 mod game_entities;
 pub use game_entities::*;
@@ -12,9 +13,20 @@ fn get_user_input() -> io::Result<(String)> {
 
 }
 
+fn print_map(map: &Array2<char>) {
+
+    for row in map.rows()  {
+        for tile in row {
+            print!("{tile}");
+        }
+        print!("\n");
+    }
+}
+
 fn main() {
     // Note: use crossterm or termion for synchronous key press event handling
     //if user_input to do: compare string, compare the input with what the string is for ESC
+
 
     // player struct
 
@@ -22,15 +34,29 @@ fn main() {
 
     let mut user_input = String::new();
 
+    // make this an array of chars
+    let mut map = Array2::<char>::default((6,8));
+
+    map.fill('#');
+    //let mut map = arr2(&[['#','#','#','#','#','#'],
+    //                       ['#','.','.','.','.','#'],
+    //                       ['#','.','.','.','.','#'],
+    //                       ['#','.','.','@','.','#'],
+    //                       ['#','.','.','.','.','#'],
+    //                       ['#','#','#','#','#','#']]);
+
     while user_input != "q" {
         user_input = get_user_input().unwrap();
+        //dbg!(&map);
+        print_map(&map);
         println!("game is running...");
         println!("You typed: {}", user_input);
-        println!("###########");
-        println!("#.........#");
-        println!("#....@....#");
-        println!("#.........#");
-        println!("###########");
+
+        //println!("###########");
+        //println!("#.........#");
+        //println!("#....@....#");
+        //println!("#.........#");
+        //println!("###########");
     }
 
 }
